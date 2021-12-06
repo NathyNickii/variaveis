3e84868b8cb25aa99edf2ec3df5dda48ac3d0864
Nathália Ramos <88409702+NathyNickii@users.noreply.github.com>
2021-12-05 23:12:57 -0300

    Add files via upload

---
+++ indice.js
@@ -0,0 +1,23 @@
+/* Tipo de variável: String -->
+Tipo String aceita: texto e caracteres especiais. */
+var frase = "Uma frase!!!!..."
+
+/* Tipos de variável: Integer (Int) -->
+Tipo Integer (int) aceita: Números inteiros. */
+var dia = 10
+
+/* Tipos de variável: Float/Double -->
+Tipo Float/Double aceita: Valores decimais, com pontos flutuantes. */
+var valor_total = 4.50
+
+/* Tipos de variável: Boolean (true/false) -->
+Tipo Boolean aceita: Comparações entre n variáveis ou condições. */
+var tem_Filhos = true
+var foi_aprovado = false
+
+// NOMES DE CONVEN
+
+//Tem a declaração de contrato de uma casa que está sendo compart:
+var isDeclaracaoCasaCompartilhada = true
+
+console.log(frase)
